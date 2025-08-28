df6808ba2d71df211c406a417ca88603fb2035d8
xenochor <annoyomous@att.net>
2025-08-27 20:41:21 -0500

    Increment counter for LKAS

--- opendbc/car/mazda/mazdacan.py
+++ opendbc/car/mazda/mazdacan.py
@@ -66,7 +66,7 @@ def create_steering_control(packer, CP, frame, apply_torque, lkas):
 def create_steering_control_cx50h(packer, CP, frame, apply_torque, lkas):
   values = {
     "CHKSUM": 0,
-    "CTR": lkas["CTR"],
+    "CTR": (lkas["CTR"] + 5) & 0xF,
     "LKAS_EFFECTIVE": 1,
     "LKAS_EFFECTIVE_INV": 0,
     "BIT_1": lkas["BIT_1"],
